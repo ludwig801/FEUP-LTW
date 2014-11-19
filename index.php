41073a2e6b9b1bca2dba2641c3eed902919da471
Kevin Amorim <kevinamorim4@gmail.com>
2014-11-19 11:53:46 +0000

    Login page

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php 
 	include('templates/header.php');
+	include_once('database/connection.php');
+
+	session_start();
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		$myusername = $_POST['username'];
+		$mypassword = $_POST['password'];
+
+		$stmt = $db->prepare("SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'");
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		$count = count($result);
+
+		if($count == 1) {
+			$_SESSION['myusername'] = $myusername;
+
+			// Redirect to user page
+			header("location: admin.php");
+		} else {
+			echo "Your username and/or password is invalid hehe";
+		}
+	}
 ?>
 
 	<h2> Login now, bithc </h2>
 
 
-	<form method="POST" action="database/validate_login.php">
+	<form method="POST" action="">
 
 		<p><input type="text" placeholder="username" name="username" /></p>
 		<p><input type="password" placeholder="password" name="password" /></p>
